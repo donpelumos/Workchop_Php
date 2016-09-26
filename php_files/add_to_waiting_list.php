d64d2b8bfb8f7ac740f569cfcb1c2ff6eb646e04
Don Pelumos <donpelumos@gmail.com>
2016-09-27 00:28:45 +0100

    sixth

---
+++ php_files/add_to_waiting_list.php
@@ -0,0 +1,27 @@
+<?php
+	$database = 'mysql:dbname=workchop_main;host=localhost;';
+	$user = 'workchop_admin';
+	$pwd = 'workchop_12345';
+	$currentDate = date("Y-m-d H:i:s");
+	
+	//phpinfo();
+	
+	try {
+		$db = new PDO($database, $user, $pwd);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+		$query = $db->prepare("insert into waiting_list(id, email_address, date_time) values(:id, :email, :date)");
+		$query->bindParam(':id',$id);
+		$query->bindParam(':email',$email);
+		$query->bindParam(':date',$currentDate);
+		
+		$id = md5(time());
+		$email = strip_tags(trim($_GET['email']));
+		$query->execute();
+		
+		echo "done--".$id;
+	} 
+	catch(PDOException $e) {
+		die('Could not connect to the database:<br/>' . $e);
+	}
+?>
